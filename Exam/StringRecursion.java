2037f3e918e2044e40d32cad68b1c80c46ec1476
ShubhamRavani <60728572+ShubhamRavani@users.noreply.github.com>
2020-02-11 11:08:53 +0530

    Add files via upload

--- Exam/StringRecursion.java
+++ Exam/StringRecursion.java
@@ -5,3 +5,16 @@ public class StringRecursion
 {
 	public static void main(String args[])
 	{
+		String str="Shubham";
+		String rev = rString(str);
+        System.out.println("Reversed string is: " +rev);
+	}
+	public static String rString(String str)
+    {
+        if (str.isEmpty())
+		{
+			return str;
+		}
+		return rString(str.substring(1)) + str.charAt(0);
+	}
+}
